44ebc85731aeafede74918bff72c05061fbc8432
Harsh D. Pandya <131833376+ha31457@users.noreply.github.com>
2026-05-03 20:53:01 +0530

    Added the first migrations and created the tables.

--- src/main/java/com/hss/scalecart/ScalecartApplication.java
+++ src/main/java/com/hss/scalecart/ScalecartApplication.java
@@ -1,7 +1,9 @@
 package com.hss.scalecart;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScalecartApplication {
@@ -9,5 +11,4 @@ public class ScalecartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScalecartApplication.class, args);
 	}
-
 }
