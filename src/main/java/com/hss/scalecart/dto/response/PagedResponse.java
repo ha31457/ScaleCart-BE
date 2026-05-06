17f11e2bf7473b21a6839130acc08a7f68649a73
Harsh D. Pandya <131833376+ha31457@users.noreply.github.com>
2026-05-06 20:44:46 +0530

    Added no args and all args constructor to these DTO

--- src/main/java/com/hss/scalecart/dto/response/PagedResponse.java
+++ src/main/java/com/hss/scalecart/dto/response/PagedResponse.java
@@ -1,15 +1,20 @@
 package com.hss.scalecart.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PagedResponse<T> {
     private List<T> items;
     private int pageSize;
     private boolean hasMore;
-    private UUID nextCursor;   // client sends this back as lastSeenId
+    private UUID nextCursor;
 }
--- src/main/java/com/hss/scalecart/dto/response/ProductResponse.java
+++ src/main/java/com/hss/scalecart/dto/response/ProductResponse.java
@@ -1,14 +1,19 @@
 package com.hss.scalecart.dto.response;
 
 import com.hss.scalecart.enums.ProductStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor      // ← add this — Jackson needs it to deserialize
+@AllArgsConstructor
 public class ProductResponse {
     private UUID id;
     private String name;
